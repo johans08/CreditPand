050c138a9bb7b3307a7e767a707a6d290100b4d4
KatheLJ <katheloajar257@gmail.com>
2021-11-08 14:53:40 -0600

    Avance Se agregó el Modelo, CRUD de Cliente y Tarjeta, Ajuste de Intereses, Login (parte) , Grid deTarjetas Activas(parte) y otras vistas.

---
+++ CreditPand.BD/Repositorios/GestorUsuario.cs
@@ -0,0 +1,110 @@
+﻿using CreditPand.BD.Interface;
+using CreditPand.BD.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditPand.BD.Repositorios
+{
+    public class GestorUsuario : IGestorUsuario
+    {
+       
+        //Método para mostrar a todos los usuarios en la vista de mantenimientos
+        IEnumerable<Usuario> IGestorUsuario.ListadoUsuarios()
+        {
+            List<Usuario> Clientes = new List<Usuario>();
+            using (CreditPandEntities ContextoBD = new CreditPandEntities())
+            {
+                Clientes= ContextoBD.Usuario.ToList();
+            }
+
+            return Clientes;
+
+        }
+
+
+        //Método para registrar a un nuevo usuario
+        int IGestorUsuario.CrearUsuario(Usuario pUsuario)
+        {
+            int n = 0;
+            using (CreditPandEntities ContextoBD = new CreditPandEntities())
+            {
+                ContextoBD.Usuario.Add(pUsuario);
+                n = ContextoBD.SaveChanges();
+            }
+            return n;
+
+        }
+
+        //Método para actualizar a un usuario en la vista de mantenimientos 
+        int IGestorUsuario.ActualizarUsuario(Usuario pUsuario)
+        {
+            int n = 0;
+            using (CreditPandEntities ContextoBD = new CreditPandEntities())
+            {
+                ContextoBD.Entry<Usuario>(pUsuario).State = System.Data.Entity.EntityState.Modified;
+                n = ContextoBD.SaveChanges();
+            }
+            return n;
+
+        }
+
+        
+        //Método para borrar a un usuario en la vista de mantenimientos
+        int IGestorUsuario.BorrarUsuario(string Username)
+        {
+            int n = 0;
+            using (CreditPandEntities ContextoBD = new CreditPandEntities())
+            {
+                var obj = ContextoBD.Usuario.Where(x => x.Username == Username).FirstOrDefault();
+                if (obj == null)
+                {
+                    n = 0;
+                }
+                else
+                {
+                    ContextoBD.Entry<Usuario>(obj).State = System.Data.Entity.EntityState.Deleted;
+                    n = ContextoBD.SaveChanges();
+                }
+            }
+            return n;
+
+        }
+
+        //Método para ingresar en sesión
+        public int Login(Usuario pUsuario)
+        {
+            int n = 0;
+            using (CreditPandEntities ContextoBD = new CreditPandEntities())
+            {
+                    var obj = ContextoBD.Usuario.Where(a => a.Username.Equals(pUsuario.Username) && a.Pass.Equals(pUsuario.Pass)).FirstOrDefault();
+            }
+            return n;
+        }
+
+
+        //Método para mostrar los datos del usuario que ingreso en sesión
+        public int Profile(int id, Usuario pUsuario)
+        {
+            int n = 0;
+            using (CreditPandEntities ContextoBD = new CreditPandEntities())
+            {
+                var info = from Usuario in ContextoBD.Usuario
+                             where pUsuario.Ide == id
+                           select Usuario;
+                var user = info.FirstOrDefault<Usuario>();
+            }
+
+            return n;
+        }
+
+
+    
+}
+
+
+
+    
+}
---
+++ CreditPand.UI/Controllers/UsuarioController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows;
+using CreditPand.BD.Interface;
+using CreditPand.BD.Modelo;
+using CreditPand.BD.Repositorios;
+
+
+namespace CreditPand.UI.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+
+        private readonly IGestorUsuario _oGestorUsuario; 
+        
+        //Constructor del Usuario
+        public UsuarioController() 
+        {
+            _oGestorUsuario = new GestorUsuario();
+        }
+
+        // GET: Usuario
+        //Muestra el formulario para el registro de un cliente
+        public ActionResult Register() 
+        {
+            ViewBag.Message = "One Page transform.";
+
+            return View();
+        }
+
+        //Realiza el registro del cliente en la BD
+        public ActionResult RegistroUsuario(Usuario pUsuario)
+        {
+            int registros = _oGestorUsuario.CrearUsuario(pUsuario);
+            //Swal.fire("Registro Exitoso"); //Alert de registro exitoso
+            return RedirectToAction("Register");
+
+        }
+
+        //Muestra el formulario para ingresar sesión
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        //Permite el Login del usuario
+        public ActionResult LoginUser(Usuario pUsuario) 
+        {
+
+
+            if (ModelState.IsValid) 
+            {
+                using (CreditPandEntities ContextoBD = new CreditPandEntities()) //No debería ir acá, solamente el if
+                {
+                   
+                var data = ContextoBD.Usuario.Where(a => a.Username.Equals(pUsuario.Username) && 
+                a.Pass.Equals(pUsuario.Pass)).ToList();
+
+                
+                if (data.Count() > 0)
+                {
+                    Session["Username"] = data.FirstOrDefault().Username;
+                    return RedirectToAction("UserDashBoard");
+                }
+                else
+                {
+                    ViewBag.error = "Fallo";
+
+                        MessageBox.Show("Usuario o contraseña incorrectos", "Intente denuevo");//Cambiar por SweetAlert
+                        return RedirectToAction("Login");
+                }
+
+                }
+            }
+            return View();
+
+        }
+
+        //Muestra el nombre del usuario que ingreso en sesión, CAMBIAR
+        public ActionResult UserDashBoard()
+        {
+            if (Session["Username"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        //Muestra el perfil del usuario que se encuentra en sesión en ese momento
+        public ActionResult UserProfile(int id, Usuario pUsuario)
+        {
+            using (CreditPandEntities ContextoBD = new CreditPandEntities())
+            {
+                var info = from Usuario in ContextoBD.Usuario
+                           where pUsuario.Ide == id
+                           select Usuario;
+                var user = info.FirstOrDefault<Usuario>();
+                return View(user);
+            }
+            
+        }
+
+
+        //Muestra la tabla con todos los clientes a los que se les puede dar mantenimiento
+        public ActionResult Mantenimientos() {
+
+            IEnumerable<Usuario> Clientes = _oGestorUsuario.ListadoUsuarios();
+            return View(Clientes);
+
+        }
+
+
+        //********************************************************
+        //Para crear un usuario en los mantenimientos, falta crear la vista del form para usarla
+        public ActionResult CrearUsuario(Usuario pUsuario)
+        {
+            int registros = _oGestorUsuario.CrearUsuario(pUsuario);
+            return RedirectToAction("Mantenimientos");
+        }
+        //********************************************************
+
+
+        //Muestra el formulario que permite modificar a un usuario
+        public ActionResult EditarUserForm(int Id)
+        {
+            Usuario obj = _oGestorUsuario.ListadoUsuarios().Where(x => x.Ide == Id).FirstOrDefault();
+            return View(obj);
+
+        }
+
+
+        //Para modificar un usuario en los mantenimientos
+        public ActionResult ModificarUsuario(Usuario pUsuario)
+        {
+            int registros = _oGestorUsuario.ActualizarUsuario(pUsuario);
+            return RedirectToAction("Mantenimientos");
+        }
+
+
+        //Para borrar un usuario en los mantemientos
+        public ActionResult BorrarUsuario(string Username)
+        {
+            int registro = _oGestorUsuario.BorrarUsuario(Username);
+            return RedirectToAction("Mantenimientos");
+
+        }
+
+
+        //Para la asignación de roles a los usuarios
+        public ActionResult Roles()
+        {
+            IEnumerable<Usuario> Clientes = _oGestorUsuario.ListadoUsuarios();
+            return View(Clientes);
+        }
+
+        
+    }
+}
